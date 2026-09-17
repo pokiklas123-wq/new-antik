f22aa010e5ff68310aac2965282cd2be3e762134
pokiklas123-wq <pokiklas123@gmail.com>
2026-09-17 20:05:59 +0100

    Remove unused fetch import from server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const fetch = require('node-fetch');
+
 
 const app = express();
 const server = http.createServer(app);
